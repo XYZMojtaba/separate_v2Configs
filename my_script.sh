2bc32fb2a2d55a2654dc594d7bf3b8f7b34017e1
XYZMojtaba <mojtabamirbaqeri961@gmail.com>
2024-06-09 14:53:58 +0330

    Create my_script.sh

---
+++ my_script.sh
@@ -0,0 +1,7 @@
+curl -O https://raw.githubusercontent.com/XYZMojtaba/separate_v2Configs/main/separate_configs.sh
+
+# Set execute permission
+chmod +x separate_configs.sh
+
+# Run the script
+sh ./separate_configs.sh
